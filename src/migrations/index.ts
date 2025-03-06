0fc6fdcdf0eedd3b4af69f03e917cef752007c05
Rodvdev <rodrigovdev01@gmail.com>
2025-03-06 11:46:31 -0500

    feat: Add database migration for initial schema setup

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250306_161556 from './20250306_161556';
+
+export const migrations = [
+  {
+    up: migration_20250306_161556.up,
+    down: migration_20250306_161556.down,
+    name: '20250306_161556'
+  },
+];
